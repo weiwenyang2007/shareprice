b195bdfff1bc7e75a5934787779bba1327801a84
AlbertYang2007 <albertyang2007@gmail.com>
2023-11-06 20:49:13 +0800

    add

--- myTrader/mySingleRun.py
+++ myTrader/mySingleRun.py
@@ -8,15 +8,17 @@ log = myLogger.setup_custom_logger(__name__)
 def my_easy_trade():
     try:    
         log.info('my_easy_trade start')
-        current_time = datetime.now().strftime("%H:%M:%S")
-        if current_time >= '09:15:00' and current_time < '15:15:00':
+        dt = datetime.now()
+        week_day = dt.weekday()
+        current_time = dt.strftime("%H:%M:%S")
+        if (current_time >= '09:15:00' and current_time < '15:15:00') and (week_day >=0 and week_day <=4):
             balance_data = sanity.sanity_check()
             if balance_data:
                 sanity.deal_with_easy_trade(balance_data)
             else:
                 log.warn('sanity_result is false, skip deal_with_easy_trade')    
         else:
-            log.info('my_easy_trade not run due to time is outof trade')
+            log.info('my_easy_trade not run due to date time is outof trade')
             
         log.info('my_easy_trade end')    
             
